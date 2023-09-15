4c9ba26636f0a1be1ab7c3b00925c76abd6cadde
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-16 00:50:36 +0530

    contact-api route

---
+++ pages/api/contact.js
@@ -0,0 +1,27 @@
+function handler(req, res) {
+	if (req.method === 'POST') {
+		const { email, name, message } = req.body;
+
+		if (
+			!email ||
+			!email.includes('@') ||
+			!name ||
+			name.trim() === '' ||
+			!message ||
+			message.trim() === ''
+		) {
+			return res.status(422).json({ message: 'Please enter valid inputs' });
+		}
+
+		const newMessage = {
+			email,
+			name,
+			message,
+		};
+
+		console.log(newMessage);
+		res
+			.status(201)
+			.json({ message: 'Message sent successfully', message: newMessage });
+	}
+}
