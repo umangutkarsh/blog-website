63f17c24a810191d74c0320ac3e54cc1ee8ded54
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-16 01:31:09 +0530

    storing data in database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
 	if (req.method === 'POST') {
 		const { email, name, message } = req.body;
 
@@ -18,6 +20,28 @@ function handler(req, res) {
 			name,
 			message,
 		};
+
+		let client;
+		try {
+			client = await MongoClient.connect(
+				'mongodb+srv://umangutkarsh_:32mvsNzFuokQ@next-blogs.8z0cfea.mongodb.net/messages?retryWrites=true&w=majority'
+			);
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ message: 'Could not connect to the database' });
+		}
+
+		const db = client.db();
+
+		try {
+			const result = await db.collection('messages').insertOne(newMessage);
+			newMessage.id = result.insertedId;
+		} catch (error) {
+			return res.status(500).json({ message: 'Could not store message' });
+		}
+
+		client.close();
 
 		console.log(newMessage);
 		res
